8eee09126b85679ff9fe558d1f6896494d8d0d0b
NoRage28 <max.chyncha@gmail.com>
2023-06-05 14:42:04 +0300

    Separate urls on auth and base urls

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("users.urls")),
+    path("api/", include("users.routes")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/docs/",
---
+++ users/routes.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("", include("users.urls.base_urls")),
+    path("auth/", include("users.urls.auth_urls")),
+
+]
